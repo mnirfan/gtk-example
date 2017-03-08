988809982752a5674f0cf87ee805842bedc82f46
nurul irfan <irfanlongstride@gmail.com>
2017-03-08 17:22:37 +0700

    add reference from stack overflow

--- slow_load_on_starting_app.py
+++ slow_load_on_starting_app.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+# Reference: 
+# http://askubuntu.com/questions/142871/how-to-run-asynchronous-tasks-in-python-gobject-introspection-apps
+
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gio, GObject, GLib
